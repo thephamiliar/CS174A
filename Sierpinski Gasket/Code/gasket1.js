2102b8f5081736e67e0df3965ac19afedc59c726
thephamiliar613 <jessicaminhuyen@yahoo.com>
2014-10-16 04:36:53 -0700

    EC 4

--- Sierpinski Gasket/Code/gasket1.js
+++ Sierpinski Gasket/Code/gasket1.js
@@ -1,7 +1,12 @@
 var canvas;
 var gl;
 var points = [];
+var snow = [];
 var NumTimesToSubdivide = 4;
+var loc;
+var matrix;
+var deg = 0;
+var type = 1;
 
 // Requirement 3. Implement the Sierpinski Gasket algorithm
 /* display one triangle*/
@@ -28,8 +33,11 @@ function divideTriangle (a,b,c,count) {
 
 function render() {
 	gl.clear(gl.COLOR_BUFFER_BIT);
-	gl.drawArrays(gl.TRIANGLES, 0, points.length);
-	gl.flush();
+	if (type == 0) {
+		gl.drawArrays(gl.TRIANGLES, 0, points.length);
+    } else {
+		gl.drawArrays(gl.LINES, 0, snow.length);
+    }
 }
 
 //Extra Credit 1. Implement an application based color variable that can be passed
@@ -38,30 +46,16 @@ function colorPicker() {
 	return vec4(Math.random(), Math.random(), Math.random(), 1.0);
 }
 
-//Extra Credit 3. Implement another fractal.
-var deg_to_rad = Math.PI / 180.0;
-var depth = 9;
- 
-function drawLine(x1, y1, x2, y2, brightness){
-	points.push(x1, y1);
-	points.push(x2, y2);
-}
-function drawTree(x1, y1, angle, depth){
-	if (depth != 0){
-		var x2 = x1 + (Math.cos(angle * deg_to_rad) * depth * 10.0);
-		var y2 = y1 + (Math.sin(angle * deg_to_rad) * depth * 10.0);
-		drawLine(x1, y1, x2, y2, depth);
-		drawTree(x2, y2, angle - 20, depth - 1);
-		drawTree(x2, y2, angle + 20, depth - 1);
+  //Extra Credit 3. Implement another fractal.
+function drawSnowflake(vertices, count){
+	for (i = 1; i < count; i++) {
+		var j = Math.floor((Math.random()*6) + 1) + 1;
+		var flake = mix(snow[i-1], vertices[j], 0.3);
+		snow.push(flake);
 	}
 }
 
-// Initialize canvas and shaders
-window.onload = function init() {
-    canvas = document.getElementById("gl-canvas");
-    gl = WebGLUtils.setupWebGL(canvas);
-    if (!gl) { alert ("WebGL isn't available"); }
-
+function gasket() {
 	// Initial triangle for Sierpinski Gasket
 	var vertices = [ vec2(-0.5,-0.5), vec2(0.5,-0.5), vec2(0.0,0.5) ];
 	divideTriangle (vertices[0], vertices[1], vertices[2], NumTimesToSubdivide);
@@ -74,9 +68,13 @@ window.onload = function init() {
     var program = initShaders( gl, "vertex-shader", "fragment-shader" );
     gl.useProgram( program );
 
+    matrix = gl.getUniformLocation(program, "vMatrix");
+    var rot = rotate(deg, 0, 1, 0);
+    gl.uniformMatrix4fv(matrix, false, flatten(rot));
+
 	//Extra Credit 1. Implement an application based color variable that can be passed
 	//through to the fragment shader.
-    var loc = gl.getUniformLocation(program, "vColor");
+    loc = gl.getUniformLocation(program, "vColor");
    	var color = colorPicker();
     if (loc != -1)
     	gl.uniform4fv(loc, color);
@@ -90,6 +88,59 @@ window.onload = function init() {
     var vPosition = gl.getAttribLocation( program, "vPosition" );
     gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
     gl.enableVertexAttribArray( vPosition );
+}
+
+function koch() {
+	// Initial triangle for Sierpinski Gasket
+	var vertices = [ vec2(1,0), vec2(0.5,0.866), vec2(-0.5,0.866),
+					 vec2(-1,0), vec2(-0.5,-0.866), vec2(0.5, -0.866) ];
+	var origin = vec2 (0.25, 0.5);
+	snow.push(origin);
+	snow.push(vertices[0]);
+	snow.push(vertices[1]);
+	snow.push(vertices[2]);
+
+		//drawSnowflake (vertices, NumTimesToSubdivide);
+	//divideTriangle (vertices[0], vertices[1], vertices[2], NumTimesToSubdivide);
+
+	// Configure WebGL
+	gl.viewport( 0, 0, canvas.width, canvas.height );
+	gl.clearColor( 1.0, 1.0, 1.0, 1.0 ); 
+
+    //  Load shaders and initialize attribute buffers
+    var program = initShaders( gl, "vertex-shader", "fragment-shader" );
+    gl.useProgram( program );
+
+    matrix = gl.getUniformLocation(program, "vMatrix");
+    var rot = rotate(deg, 0, 1, 0);
+    gl.uniformMatrix4fv(matrix, false, flatten(rot));
+
+	//Extra Credit 1. Implement an application based color variable that can be passed
+	//through to the fragment shader.
+    loc = gl.getUniformLocation(program, "vColor");
+   	var color = colorPicker();
+    if (loc != -1)
+    	gl.uniform4fv(loc, color);
+    
+    // Load the data into the GPU
+    var vBufferID = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, vBufferID );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(snow), gl.STATIC_DRAW );
+
+    // Associate out shader variables with our data buffer
+    var vPosition = gl.getAttribLocation( program, "vPosition" );
+    gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vPosition );
+
+}
+
+// Initialize canvas and shaders
+window.onload = function init() {
+    canvas = document.getElementById("gl-canvas");
+    gl = WebGLUtils.setupWebGL(canvas);
+    if (!gl) { alert ("WebGL isn't available"); }
+
+    koch();
 
 	window.onkeydown = function (e){
     	var key = e.keyCode ? e.keyCode : e.which;
@@ -97,17 +148,25 @@ window.onload = function init() {
     	//Extra Credit 2. Implement	a method where the keyboard is used	to
     	//change that color variable and redisplay.
 		if (key == 32) {
-	    	var loc = gl.getUniformLocation(program, "vColor");
 		   	var color = colorPicker();
-		    if (loc != -1)
-	 		   	gl.uniform4fv(loc, color);
+	 		gl.uniform4fv(loc, color);
  			render();
 		} 
 		// Extra Credit 3. Switch between the two fractals
 		else if (key == 37) {
+			gasket();
+			type = 0;
 			render();
 		} else if (key == 39) {
+			koch();
+			type = 1;
 			render();
+		// Extra Credit 4. Rotate fractal
+		}  else if (key == 82) {
+			deg += 30;
+			var rot = rotate(deg, 0, 0, 1);
+		    gl.uniformMatrix4fv(matrix, false, flatten(rot));
+		    render();
 		}
 	}
 
