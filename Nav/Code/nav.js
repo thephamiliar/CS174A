662cdca9076bd23a697c0a14422bff96e0839c51
thephamiliar613 <jessicaminhuyen@yahoo.com>
2014-10-24 00:15:38 -0700

    8 cubes

--- Nav/Code/nav.js
+++ Nav/Code/nav.js
@@ -1,8 +1,29 @@
 var canvas;
 var gl;
-var loc;
-var matrix;
+
+var numCubes = 8;
 var numVertices = 36;
+var vertices = [
+    vec3(-0.5, -0.5, 0.5),
+    vec3(-0.5, 0.5, 0.5),
+    vec3(0.5, 0.5, 0.5),
+    vec3(0.5, -0.5, 0.5),
+    vec3(-0.5, -0.5, -0.5),
+    vec3(-0.5, 0.5, -0.5),
+    vec3(0.5, 0.5, -0.5),
+    vec3(0.5, -0.5, -0.5),
+  ];
+var vertexColors = [
+    [ 0.0, 0.0, 0.0, 1.0 ],     //black
+    [ 1.0, 0.0, 0.0, 1.0 ],     //red
+    [ 1.0, 1.0, 0.0, 1.0 ],     //yellow
+    [ 0.0, 1.0, 0.0, 1.0 ],     //green
+    [ 0.0, 0.0, 1.0, 1.0 ],     //blue
+    [ 1.0, 0.0, 1.0, 1.0 ],     //magenta
+    [ 1.0, 1.0, 1.0, 1.0 ],     //white
+    [ 0.0, 1.0, 1.0, 1.0 ]      //cyan
+  ];
+
 var indices = [
     1, 0, 3,
     3, 2, 1,
@@ -17,62 +38,86 @@ var indices = [
     5, 4, 0,
     0, 1, 5
   ];
-  var iBuffer;
+
+var near = 0.1;
+var far = 10.0;
+var fovy = 45.0;
+var aspect;
+
+var modelView, projection;
+var modelViewLoc, projLoc;
+
+var scale_cube = vec3(0.5, 0.5, 0.5);
+var translateMatrix = [
+    vec3(1.0, 1.0, -1.0),    //upper right
+    vec3(0.0, 1.0, 1.0),    //upper middle
+    vec3(-1.0, 1.0, -1.0),   //upper left
+    vec3(1.0, -1.0, -1.0),   //lower right
+    vec3(-1.0, -1.0, -1.0),  //lower left
+    vec3(1.0, 0.0, 1.0),    //middle right
+    vec3(0.0, -1.0, 1.0),   //lower middle
+    vec3(-1.0, 0.0, 1.0)    //middle left
+];
 
 window.onload = function init() {
 	canvas = document.getElementById("gl-canvas");
+
 	gl = WebGLUtils.setupWebGL(canvas);
 	if (!gl) { alert("WebGL isn't available"); }
-
-var vertices = [
-    vec3(-0.5, -0.5, 0.5),
-    vec3(-0.5, 0.5, 0.5),
-    vec3(0.5, 0.5, 0.5),
-    vec3(0.5, -0.5, 0.5),
-    vec3(-0.5, -0.5, -0.5),
-    vec3(-0.5, 0.5, -0.5),
-    vec3(0.5, 0.5, -0.5),
-    vec3(0.5, -0.5, -0.5),
-  ];
-
-  var vertexColors = [
-    [ 0.0, 0.0, 0.0, 1.0 ],
-    [ 1.0, 0.0, 0.0, 1.0 ],
-    [ 1.0, 1.0, 0.0, 1.0 ],
-    [ 0.0, 1.0, 0.0, 1.0 ],
-    [ 0.0, 0.0, 1.0, 1.0 ],
-    [ 1.0, 0.0, 1.0, 1.0 ],
-    [ 1.0, 1.0, 1.0, 1.0 ],
-    [ 0.0, 1.0, 1.0, 1.0 ]
-  ];
   
 	// Configure WebGL
 	gl.viewport( 0, 0, canvas.width, canvas.height );
 	gl.clearColor( 1.0, 1.0, 1.0, 1.0 ); 
+    aspect = canvas.width/canvas.height;
+
+    gl.enable(gl.DEPTH_TEST);
 
     //  Load shaders and initialize attribute buffers
     var program = initShaders( gl, "vertex-shader", "fragment-shader" );
     gl.useProgram( program );
+
+    // array element buffer
+    var iBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, iBuffer);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint8Array(indices), gl.STATIC_DRAW);
+
+    // color array attribute buffer
+    var cBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(vertexColors), gl.STATIC_DRAW);
+
+    var vColor = gl.getAttribLocation(program, "vColor");
+    gl.vertexAttribPointer(vColor, 4, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vColor);
     
-    // Load the data into the GPU
-    var vBufferID = gl.createBuffer();
-    gl.bindBuffer( gl.ARRAY_BUFFER, vBufferID );
+    // vertex array attribute buffer
+    var vBuffer = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer );
     gl.bufferData( gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW );
-    gl.bufferData( gl.ARRAY_BUFFER, flatten(vertexColors), gl.STATIC_DRAW );
 
-    iBuffer = gl.createBuffer();
-    gl.bindBuffer( gl.ELEMENT_ARRAY_BUFFER, iBuffer);
-    gl.bufferData( gl.ELEMENT_ARRAY_BUFFER, flatten(indices), gl.STATIC_DRAW);
-
-    // Associate out shader variables with our data buffer
     var vPosition = gl.getAttribLocation( program, "vPosition" );
-    gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
+    gl.vertexAttribPointer( vPosition, 3, gl.FLOAT, false, 0, 0 );
     gl.enableVertexAttribArray( vPosition );
+
+    // uniform variables
+    modelViewLoc = gl.getUniformLocation(program, "modelView");
+    projLoc = gl.getUniformLocation(program, "projection");
 
     render();
 }
 
 function render() {
-	gl.clear(gl.COLOR_BUFFER_BIT);
-    gl.drawElements(gl.TRIANGLES, numVertices, gl.UNSIGNED_BYTE, 0);
+	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    for (var i=0; i < numCubes; ++i) {
+        var ctm = mat4();
+        ctm = mult(ctm, rotate(30, 1, 1, 0));
+        ctm = mult(ctm, scale(scale_cube));
+        ctm = mult(ctm, translate(translateMatrix[i]));
+        projection = perspective(fovy, aspect, near, far);
+        gl.uniformMatrix4fv(modelViewLoc, false, flatten(ctm));
+        gl.uniformMatrix4fv(projLoc, false, flatten(projection));
+
+        gl.drawElements(gl.TRIANGLES, numVertices, gl.UNSIGNED_BYTE, 0);
+    }
 }
